405270a032a325b93b60321f9699d988174eacb4
Gavi5d <sjqbuaa@gmail.com>
2017-08-28 19:36:18 +0800

    add some files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,45 @@
+
+// grab our gulp packages
 var gulp = require("gulp");
 var gutil = require("gulp-util");
+var jshint = require("gulp-jshint");
+var sourcemaps = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var uglify = require("gulp-uglify");
+
+gulp.task("default", ["watch"]);
+
+gulp.task("jshint", function () {
+    return gulp.src("source/javascript/**/*.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter("jshint-stylish"));
+});
+
+gulp.task("build-css", function () {
+    return gulp.src("source/scss/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("public/assets/stylesheets"));
+});
+
+gulp.task("build-js", function () {
+    return gulp.src("source/javascript/**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(gutil.env.type === "production" ? uglify() : gutil.noop())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("public/assets/javascript"));
+});
+
+gulp.task("watch", function () {
+    gulp.watch("source/javascript/**/*.js", ["jshint"]);
+    gulp.watch("source/scss/**/*.scss", ["build-css"]);
+});
+
+
+
+
+
+
 
 
---
+++ public/assets/javascript/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+for (var i = 0; i < 5; i += 1) {
+    console.log(i);
+}
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiO1xyXG5cclxuZm9yICh2YXIgaSA9IDA7IGkgPCA1OyBpICs9IDEpIHtcclxuICAgIGNvbnNvbGUubG9nKGkpO1xyXG59XHJcbiJdLCJmaWxlIjoibWFpbi5qcyJ9
